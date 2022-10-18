44be80135728b8dce9a48b2d1f6dc90a3d42efec
sdfy-fly <rozhov25@bk.ru>
2022-10-18 08:31:19 +0300

    upd schedule

--- service/schedule.py
+++ service/schedule.py
@@ -1,19 +1,29 @@
 import aiohttp
 from bs4 import BeautifulSoup
 
-
 async def getSchedule():
     
     url = 'https://www.rotowire.com/baseball/projected-starters.php'
     auth = {'username' : 'Keysik' , 'password' : 'Fantasymlb' }
+    data = {'data' : ""}
     async with aiohttp.ClientSession() as session:
 
-        async with session.post('https://www.rotowire.com/users/login.php', data = auth) as r : 
-            
-            await r.text()
+        async def auth_(session:aiohttp.ClientSession , auth):
 
-            async with session.get(url=url, data = auth) as response : 
+            async with session.post('https://www.rotowire.com/users/login.php', data = auth) as r : 
+                
+                await r.text()
+
+        async def getData(session:aiohttp.ClientSession , url):
+
+            async with session.get(url=url) as response : 
 
                 soup = BeautifulSoup(await response.text() , 'html.parser')
-                data = soup.find(class_='starters-matrix mb-10')
-                return {'data' : data}
+                data["data"] = soup.find(class_='starters-matrix mb-10')
+                print(len(soup.find_all(class_='flex-row myleagues__proteam')))
+                return data
+                
+        await auth_(session,auth)
+        await getData(session,url) 
+
+        return data
