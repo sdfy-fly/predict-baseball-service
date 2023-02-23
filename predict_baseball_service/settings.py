42adfca29d6626900ce67ec713f6584c4dc8f5e7
sdfy-fly <rozhov25@bk.ru>
2023-02-23 17:33:26 +0300

    static root

--- predict_baseball_service/settings.py
+++ predict_baseball_service/settings.py
@@ -107,6 +107,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/home/backend/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
